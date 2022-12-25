6767a101ecc0c23a7547aff7196d0854e05c6fe2
Vladislav.M <lidpid@gmail.com>
2022-12-25 20:23:44 +0600

    extra task corrected

--- Task_extra/Program.cs
+++ Task_extra/Program.cs
@@ -1,13 +1,17 @@
 ﻿Console.Clear();
 Console.WriteLine("Вводите числа");
 int a = Convert.ToInt32(Console.ReadLine());
-int max = 0;
+int max = a; 
 int max2 = 0;
 while(a != 0){
-    a = Convert.ToInt32(Console.ReadLine());
+ a = Convert.ToInt32(Console.ReadLine());
     if(a > max){
         max2 = max;
         max = a;
     }
+    else if(a > max2){
+        max2 = a;
+    }
 }
 Console.WriteLine($"Второй максимум: {max2}");
+Console.WriteLine($"Максимум: {max}");
